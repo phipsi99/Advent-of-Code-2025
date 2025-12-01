f1584aca60faae8c2be676172b46126a4bac054e
phipsi99 <schoppphilipp@googlemail.com>
2025-12-01 08:58:18 +0100

    day 1 partially

--- 01/main.py
+++ 01/main.py
@@ -1,17 +1,37 @@
 from pathlib import Path
 
+
 def do_main(debug_mode=False):
-    with open(Path('01/input.txt')) as file:
+    with open(Path("01/input.txt")) as file:
         lines = [line.rstrip() for line in file]
-    
+
     if debug_mode:
-        with open(Path('01/test.txt')) as file:
+        with open(Path("01/test.txt")) as file:
             lines = [line.rstrip() for line in file]
 
     point_sum = 0
+    point_sum2 = 0
+
+    pointer = 0
 
     for line_index, line in enumerate(lines):
-        r = [int(i) for i in line.split(" ")]
+        num = int(line[1:])
+        if line.startswith("L"):
+            if pointer - num < 0:
+                x = 100 if pointer != 0 else 0
+                point_sum2 += (x + abs(pointer - num)) // 100
+            pointer = (pointer - num) % 100
+        else:
+            if pointer + num > 99:
+                point_sum2 += (pointer + num) // 100
+            pointer = (pointer + num) % 100
 
-if __name__ == '__main__':
+        if pointer == 0:
+            point_sum += 1
+
+    print(point_sum)
+    print(point_sum2)
+
+
+if __name__ == "__main__":
     do_main(False)
